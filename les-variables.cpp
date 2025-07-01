49105ce77b1f2a3bd0235f52e080fc37936018b9
Electro Robot <training@electro-robot.com>
2025-07-02 00:03:31 +0200

    Add files via upload

---
+++ les-variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//déclaration des variables:
+	int x = 100;
+	double montant = 2;
+	string Message = "Ceci est un exemple";
+	
+	//Affichage des variables
+	cout << "X = " << x << endl;
+	cout << "montant = " << montant << endl;
+	cout << "Message est : " << Message << endl;
+
+  // terminate the program:
+  return 0;
+}
